3ee2a12121542133c651a9f2b1af2ab794fbe919
Paulo André <pauloandre7@users.noreply.github.com>
2024-02-22 11:40:23 -0300

    Modified Main class to consume API

--- src/main/java/com/pauloandre7/consumingrest/ConsumingRestApplication.java
+++ src/main/java/com/pauloandre7/consumingrest/ConsumingRestApplication.java
@@ -1,13 +1,37 @@
 package com.pauloandre7.consumingrest;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConsumingRestApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(ConsumingRestApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumingRestApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder){
+		return builder.build();
+	}
+
+	@Bean
+	@Profile("!test")
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Joke joke = restTemplate.getForObject(
+				"http://127.0.0.1:8080/random-joke", Joke.class);
+				log.info(joke.toString());
+		};
+	}
 }
