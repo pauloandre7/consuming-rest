5dd3340a6708ec683281b00278c9280cdf04e679
Paulo André <pauloandre7@users.noreply.github.com>
2024-02-22 11:09:32 -0300

    init project

---
+++ src/test/java/com/pauloandre7/consumingrest/ConsumingRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pauloandre7.consumingrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
